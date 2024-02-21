2fcc3410d3ef9a0076e637b7cdb00e4d4850ab69
Anjali Bhawari <anjalibhawari1810@gmail.com>
2024-02-21 10:33:37 +0530

    first

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,26 @@
+// Created API
+const  userRoutes = require('./routes/userRoutes');
 const notes = require('./data/notes');
 const express = require("express");  //import Express
 const dotenv = require('dotenv');
+const { connect } = require('mongoose');
+const connectDB= require("./config/db");
+const { notFound, errorHandler } = require('./middlewares/errorMiddleware');
 
+
+// var corsOptions = {
+//     origin: 'http://localhost:3000',
+//     optionSuccessStatus:200,
+//     methods:"GET,PUT,POST"
+// } 
+// app.use(cors(corsOptions));
 const app = express();
 dotenv.config();
+connectDB();
 
-//brings data from backed to frontend & serves it
+app.use(express.json());
+
+//get = brings data from backed to frontend & serves it
 app.get("/",(req,res)=>{
     res.send("API is running..");                              //create api endpoint 
 });
@@ -14,11 +29,16 @@ app.get('/api/notes',(req,res) =>{
     res.json(notes);        //all data is fectched
 });
 
-app.get('/api/notes/:id', (req,res)=>{                       //only one notes is fectched with id
+app.use('/api/users',userRoutes);
+
+/*app.get('/api/notes/:id', (req,res)=>{                       //only one notes is fectched with id
     const note = notes.find((n) => n._id === req.params.id);       
     res.send(note);
-});
+}); */
 
-const PORT= process.env.PORT || 5000;                //Port from env file - it will use port from env file not found then it run on 5000
+app.use(notFound);//middleWares
+app.use(errorHandler);
+
+const PORT= process.env.PORT || 5000; //Port from env file-it will use port from env file not found then it run on 5000
 
 app.listen(PORT,console.log(`Server started on PORT ${PORT}`)); //web Server is Created 
